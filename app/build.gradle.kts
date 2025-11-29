93b72b2751482c94d22078f31f976c74d4e4d346
TitanX <o.xtcao@gmail.com>
2025-11-29 21:47:54 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
             enableV3Signing = false
             enableV4Signing = false
         }
-    }   
+    }
 
     defaultConfig {
         applicationId = "xyz.wingio.logra"
@@ -35,10 +35,9 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt")))
-
             isUseProguard = true
         }
-    }   
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
