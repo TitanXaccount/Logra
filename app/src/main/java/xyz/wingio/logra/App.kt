02b5d042f0445a7b2085a98e3bfd671d5a61ef4a
TitanX <o.xtcao@gmail.com>
2025-11-29 15:49:39 +0100

    Create App.kt

---
+++ app/src/main/java/xyz/wingio/logra/App.kt
@@ -0,0 +1,15 @@
+package xyz.wingio.logra
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            // Remove modules(appModule) if it doesn't exist
+        }
+    }
+}
